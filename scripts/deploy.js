ba8cd8e6c4bfc56ea549eb1de1d53ac0e30642f8
Dilan Tompkins <65461019+TomTerrific7@users.noreply.github.com>
2021-09-07 02:22:01 -0400

    updated deploy scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,20 +6,14 @@
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const ticketPrice = ethers.utils.parseEther(".25")
+  const totalNumOfPlayers = 6;
+  const Contract = await hre.ethers.getContractFactory("Rroulette");
+  const contract = await Contract.deploy(ticketPrice, totalNumOfPlayers);
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await contract.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Roulette deployed to:", contract.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- test/test.js
+++ test/test.js
@@ -1,19 +1,97 @@
-const { expect } = require("chai");
+const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+//These test scripts need a lot of work 
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+describe("Rroulette", function () {
+  it("Should deploy the contract", async function () {
+    const initialSupply = ethers.utils.parseEther("");
+    const Roulette = await ethers.getContractFactory("Rroulette");
+    const roulette = await Roulette.deploy();
+    await roulette.deployed()
   });
+
+  it(" contract should be funded initially", async function() {
+    let balance = await ethers.provider.getBalance(roulette.address);
+    assert.equal(balance.toString(), initialSupply.toString());
+  });
+
+
+  describe("creating a game", function () {
+    it('Should require player has sufficient funds', async function()  {
+      const ticketPrice = ethers.utils.parseEther(".25")
+      const player1 = players[0]
+      const players = await roulette.players()
+     await roulette.deployed() 
+     assert(await roulette.balanceOf(player1) >= ticketPrice)
+  
 });
+
+it('should accept payment from initial player', async () => {
+   await roulette.createNewGame({
+    from: players[0],
+    value: ('0.25', 'ether')
+})
+});
+    
+       
+describe("joining a newly created game", function() {
+
+  it('Should allow other players to join', async () => {
+    const players = await roulette.players()
+    await roulette.createNewGame()
+     assert(await players == 5)
+     await roulete.joinGame({
+       from: players[1,2,3,4],
+       value:('.25', 'ether')
+     
+     })
+    });
+  
+
+  describe("starting and playing the game", function() {
+    it("should update the balance of the contract", async () => {
+      
+      const balance = await ethers.provider.getBalance(roulette.address)
+      const expectedBalance = ethers.utils.parseEther("1"); //.25 x 5
+      assert.equal(balance.toString(), expectedBalance.toString());
+    });
+
+    it("should initiate first round, spin chamber, and eliminate 1 player"), async () => {
+      const spinChamber = await roulette.spinChamber()
+      const players = await roulette.players()
+      const pullTrigger = await roulette.pullTrigger()
+       await roulette.startGame()
+      assert(spinChamber)
+      for(i=0; i = players.length; i++){
+        if(pullTrigger == false){
+          await roulettte.nextPlayer()
+        }else{
+          players--
+        }
+      }
+
+    
+      it("should initiate round 2, spin chamber, and eliminate another player"), async () => {
+        expect(players == 4)
+        await roulette.startGame()
+      assert(spinChamber)
+      for(i=0; i = players.length; i++){
+        if(pullTrigger == false){
+          await roulettte.nextPlayer()
+        }else{
+          players--
+        }
+      }
+    
+
+      describe("Paying winner", function () {
+        it("Should pay the winner at the end of the game", async function () {
+          //
+        
+      
+      
+    
+
+  
+
