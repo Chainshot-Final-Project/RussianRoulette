76529f47f1364d1b0a85af4cfef197f7ebee2637
Dilan Tompkins <65461019+TomTerrific7@users.noreply.github.com>
2021-09-07 21:54:04 -0400

    updated test cases

--- test/test.js
+++ test/test.js
@@ -7,6 +7,10 @@ const { ethers } = require("hardhat");
 describe("Rroulette", () => {
   let contract;
   let owner;
+  const vrfCoordinator = 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B
+  const LinkToken = 0x01BE23585060835E02B77ef475b0Cc51aA1e0709
+  const KeyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311
+  const fee = 0.1 * 10 ** 18;
   
  beforeEach(async () => {
    let ticketPrice = await contract.ticketPrice;
@@ -19,13 +23,13 @@ describe("Rroulette", () => {
     
   });
   describe("Deployment", () => {
-    
+   
     it('Should set the owner', async () => {
       expect(await contract.owner().to.equal(owner.address));
     });
 
     it('Should set the VRF Coordinator', async () => {
-
+expect(await vrfCoordinator)
     });
 
     it('Should set the Link token address', async () => {
@@ -47,6 +51,7 @@ describe("Rroulette", () => {
   })
 
   describe("creating a game",  () => {
+
     it('Should require player has sufficient funds', async () =>  {
      await contract.deployed();
      await contract.createNewGame() 
@@ -54,12 +59,33 @@ describe("Rroulette", () => {
   
 });
 
-it('should accept payment from initial player', async () => {
-   
+it('should accept payment from initial player', async () => {  
   await contract.createNewGame({
     from: players[0],
     value: ticketPrice, 
-})
+});
+
+
+});
+
+describe("joining a game",  () => {
+
+  it('Should require that a game is already created', async () =>  {
+   
+});
+
+it('Should require that the player has sufficient balance', async () =>  {
+   
+});
+
+it('Should require that the game is not full', async () =>  {
+   
+});
+
+it('Should start the game once max numOfPlayers is reached', async () =>  {
+   //also changes state
+});
+
 });
  });
   });
